4763ec772fcd88836e804b8f66744c3cb93eb3e1
Shankar Raja <themetaversemaker@gmail.com>
2023-08-15 22:16:48 +0530

    list updates

--- Lists/Lists/Program.cs
+++ Lists/Lists/Program.cs
@@ -34,6 +34,34 @@
             {
                 Console.WriteLine(name + " has penalty points");
             }
+            Console.WriteLine("--------------------------------");
+            List<string>driverSquad2 = new List<string>() { "Verstappen","Norris","Alonso","Ocon","Gasly"};
+            Console.WriteLine("Capacity of second List :: "+driverSquad2.Capacity);
+            drivers.AddRange(driverSquad2);
+            Console.WriteLine("------------AFTER MERGING TWO LISTS------------------------");
+            foreach (string driver in drivers)
+            { Console.WriteLine(driver); }
+            drivers.Sort();
+            Console.WriteLine("------------DRIVERS AFTER SORTING------------------------");
+            foreach (string driver in drivers)
+            { Console.WriteLine(driver); }
+            Console.WriteLine("Position of Ocon :: "+ drivers.BinarySearch("Ocon"));
+            Console.WriteLine("------------REMOVING A DRIVER------------------------");
+            Console.WriteLine("Enter the position number :: ");
+            string id = Console.ReadLine();
+            int val = int.Parse(id);
+            string driverName = drivers[val].ToString();
+            drivers.Remove(driverName);
+            Console.WriteLine("------------AFTER REMOVE OPERATION------------------------");
+            foreach (string driver in drivers)
+            { Console.WriteLine(driver); }
+            Console.WriteLine("------------REMOVING A SERIES OF DRIVER------------------------");
+            Console.WriteLine("Enter the start and total count number :: ");
+            int startVal = int.Parse(Console.ReadLine());
+            int endVal = int.Parse(Console.ReadLine());
+            drivers.RemoveRange(startVal, endVal);
+            foreach (string driver in drivers)
+            { Console.WriteLine(driver); }
         }
     }
 }
