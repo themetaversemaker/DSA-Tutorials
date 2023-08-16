f17ec1a624d5be30646d2f4c170df1f3b6962cee
Shankar Raja <themetaversemaker@gmail.com>
2023-08-16 21:18:00 +0530

    task1

---
+++ Stack/Push Pop Challenge/Push Pop Challenge/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// A letter means push and an asterisk means pop in the
+// following sequence.  Give the sequence of values returned by the pop operations
+// when this sequence of operations is performed on an initially empty LIFO stack.
+// E A S * Y * Q U E * * * S T * * * I O * N * * *
+namespace Push_Pop_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = new char[] { 'E','A','S','*','Y','*','Q','U','E','*','*','*','S','T','*','*','*','I','O','*','N','*','*','*' };
+            Stack<char> inputPattern = new Stack<char>() { };
+            foreach (char c in arr)
+            {
+                if (c != '*') 
+                {
+                    inputPattern.Push(c);
+                }
+                else
+                {
+                    Console.WriteLine($"Element Popped :: {inputPattern.Pop()}");
+                }
+            }
+
+        }
+    }
+}
