d55e0986cef31b6b43c829b9f1154991360e4773
Shankar Raja <themetaversemaker@gmail.com>
2023-08-17 13:40:08 +0530

    Queue

---
+++ Queue/Queue Operations/Queue Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Queue_Operations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int totalDrivers = 0;
+            Queue<string> drivers = new Queue<string>();
+            Console.WriteLine("Enter the total drivers :: ");
+            totalDrivers = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter the name of {totalDrivers} drivers ");
+            while(count!=totalDrivers)
+            {
+                drivers.Enqueue(Console.ReadLine());
+                count++;
+            }
+            Console.WriteLine($"Peek operation :: {drivers.Peek()}");
+            Console.WriteLine($"First Out :: {drivers.Dequeue()}");
+
+        }
+    }
+}
