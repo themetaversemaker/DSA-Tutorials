5a731967bbd807f1514a0efdad5c5df821cfedf5
Shankar Raja <themetaversemaker@gmail.com>
2023-08-26 20:28:00 +0530

    linear search

---
+++ Linear Search/Linear Search/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Linear_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int count = 0;
+            Console.WriteLine("Linear Search\n-------\n");
+            Console.WriteLine("Player 1 :: Enter three driver names");
+            while (count != 3)
+            {
+                list.Add(Console.ReadLine());
+                count++;
+            }
+            Console.Clear();
+            Console.WriteLine("Player 2 :: Enter a driver name :: ");
+            string name = Console.ReadLine();
+            bool result = LinearSearch(list, name);
+            Console.Clear();
+            if(result)
+            {
+                Console.WriteLine("CORRECT! You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("OOPS! You didn't get it right");
+            }
+        }
+
+        private static bool LinearSearch(List<string> list, string name) 
+        {
+            foreach(string item in list)
+            {
+                if(item == name)
+                    return true;
+            }
+            return false;
+        }
+    }
+
+}
